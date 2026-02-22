240cc68b5fcc54f73801810b4949e65803a2a053
Atlasov Rayan <dotarayanru03@gmail.com>
2026-02-22 20:25:53 +0300

    Minor changes were made to the styles and the logo was changed

--- web/src/widgets/header/ui/HeaderLogo.tsx
+++ web/src/widgets/header/ui/HeaderLogo.tsx
@@ -1,9 +1,9 @@
-import {RiHome9Line} from "react-icons/ri";
+import { BsHouseCheck } from "react-icons/bs";
 
 export default function HeaderLogo() {
     return (
         <div className="header__logo">
-            <RiHome9Line className="header__logo-icon"/>
+            <BsHouseCheck className="header__logo-icon"/>
             <span className="header__logo-text">
                 Мой Дом
             </span>
