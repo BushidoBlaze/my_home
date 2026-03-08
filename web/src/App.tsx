0ce0a8a2c3dee6abbb5327ac4c5065ae37e4b029
Atlasov Rayan <dotarayanru03@gmail.com>
2026-03-08 22:06:21 +0300

    A new 404 page has been added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
 // plugins
 import {BrowserRouter, Routes, Route} from "react-router";
 
+// hooks
+import {useScrollReveal} from "@/shared/hooks/useScrollReveal.ts";
+
 // routs
 import MainLayout from "./layouts/MainLayout.tsx";
 
 // pages
 import Home from "./pages/Home.tsx";
+import NotFound from "./pages/notFound/NotFound.tsx"; // 404
 
 // styles
 import "./shared/assets/styles/reset.css";
@@ -16,12 +20,16 @@ import "./shared/assets/styles/fonts.css";
 const basename = import.meta.env.PROD ? '/my_home' : '/';
 
 export default function App() {
+    useScrollReveal();
+
     return (
         <BrowserRouter basename={basename}>
             <Routes>
                 <Route element={<MainLayout/>}>
                     <Route path="/" element={<Home/>}/>
                 </Route>
+
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
         </BrowserRouter>
     )
